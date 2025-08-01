e7fe032bb5d6ed291a784123730e8a673dea495f
Marvin Truong <khatruong06@gmail.com>
2025-08-01 15:30:49 +1000

    Dev:  socket header

--- src/services/socket/Socket.hpp
+++ src/services/socket/Socket.hpp
@@ -1,1 +1,23 @@
-// Socket.hpp
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
+#include <netinet/in.h>
+
+class Socket
+{
+public:
+    Socket(int port);
+    ~Socket();
+
+    int getServerSocket();
+
+    void listenSocket();
+    void closeSocket();
+
+private:
+    int serverSocket;
+    sockaddr_in serverAddress;
+    int port;
+};
+
+#endif
